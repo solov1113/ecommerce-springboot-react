66bc3e59e1455832815781d739b3cbbf60400865
ericdev0212 <eric.devos0212@gmail.com>
2023-02-22 18:18:16 +0100

    Catalog services implementations init

---
+++ catalog-service/src/main/java/catalogservice/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package catalogservice.service.impl;
+
+import catalogservice.entity.Category;
+import catalogservice.repository.CategoryRepository;
+import catalogservice.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category create(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+}
