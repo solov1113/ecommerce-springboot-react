d2a735c252a337de7f8af3c8c60e7432f276aebd
ericdev0212 <eric.devos0212@gmail.com>
2023-02-22 18:16:37 +0100

    Catalog repositories init

---
+++ catalog-service/src/main/java/catalogservice/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package catalogservice.repository;
+
+import catalogservice.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
