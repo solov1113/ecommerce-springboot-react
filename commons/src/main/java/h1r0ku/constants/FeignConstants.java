87ea97c63f69b096a6690f64fc130d113f0a2f08
ericdev0212 <eric.devos0212@gmail.com>
2023-02-23 20:42:58 +0100

    Add constants

---
+++ commons/src/main/java/h1r0ku/constants/FeignConstants.java
@@ -0,0 +1,6 @@
+package h1r0ku.constants;
+
+public class FeignConstants {
+    public static final String IMAGE_SERVICE = "image-service";
+    public static final String CATALOG_SERVICE = "catalog-service";
+}
---
+++ commons/src/main/java/h1r0ku/constants/PathConstants.java
@@ -0,0 +1,6 @@
+package h1r0ku.constants;
+
+public class PathConstants {
+    public static final String API_V1 = "/api/v1";
+    public static final String API_V1_IMAGE = API_V1 + "/images";
+}
