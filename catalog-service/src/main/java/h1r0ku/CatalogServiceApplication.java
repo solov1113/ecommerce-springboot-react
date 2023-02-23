9d85af52803c01529ab9fa416129951e5edb584c
ericdev0212 <eric.devos0212@gmail.com>
2023-02-23 20:36:32 +0100

    Enable feign clients

--- catalog-service/src/main/java/h1r0ku/CatalogServiceApplication.java
+++ catalog-service/src/main/java/h1r0ku/CatalogServiceApplication.java
@@ -3,9 +3,11 @@ package h1r0ku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
