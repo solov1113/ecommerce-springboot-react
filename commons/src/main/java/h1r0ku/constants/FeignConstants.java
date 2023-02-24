f3e6838febecfda87806150fb930c1176daf9982
ericdev0212 <eric.devos0212@gmail.com>
2023-02-24 19:45:12 +0100

    Add constants for catalog, customer, order and auth

--- commons/src/main/java/h1r0ku/constants/FeignConstants.java
+++ commons/src/main/java/h1r0ku/constants/FeignConstants.java
@@ -3,4 +3,7 @@ package h1r0ku.constants;
 public class FeignConstants {
     public static final String IMAGE_SERVICE = "image-service";
     public static final String CATALOG_SERVICE = "catalog-service";
+    public static final String CUSTOMER_SERVICE = "customer-service";
+    public static final String ORDER_SERVICE = "order-service";
+    public static final String AUTH_SERVICE = "auth-service";
 }
--- commons/src/main/java/h1r0ku/constants/PathConstants.java
+++ commons/src/main/java/h1r0ku/constants/PathConstants.java
@@ -2,5 +2,17 @@ package h1r0ku.constants;
 
 public class PathConstants {
     public static final String API_V1 = "/api/v1";
+
     public static final String API_V1_IMAGE = API_V1 + "/images";
+
+    public static final String API_V1_CATALOG = API_V1 + "/catalog";
+    public static final String API_V1_CATEGORY = API_V1_CATALOG + "/categories";
+    public static final String API_V1_PRODUCT = API_V1_CATALOG + "/products";
+
+    public static final String API_V1_CUSTOMER = API_V1 + "/customers";
+
+    public static final String API_V1_ORDER = API_V1 + "/orders";
+
+    public static final String API_V1_AUTH = API_V1 + "/auth";
+
 }
