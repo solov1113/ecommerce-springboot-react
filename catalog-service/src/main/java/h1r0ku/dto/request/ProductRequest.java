14b914d5a3f12c95bbc543c9b18fd4ebc9e10257
ericdev0212 <eric.devos0212@gmail.com>
2023-02-24 19:46:12 +0100

    Add few more variables & change price type to BigDecimal

--- catalog-service/src/main/java/h1r0ku/dto/request/ProductRequest.java
+++ catalog-service/src/main/java/h1r0ku/dto/request/ProductRequest.java
@@ -3,11 +3,14 @@ package h1r0ku.dto.request;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 public class ProductRequest {
-    private Double price;
+    private String productName;
+    private String description;
+    private BigDecimal price;
     private Long categoryId;
     private List<MultipartFile> images;
 }
--- catalog-service/src/main/java/h1r0ku/dto/response/ProductResponse.java
+++ catalog-service/src/main/java/h1r0ku/dto/response/ProductResponse.java
@@ -2,13 +2,17 @@ package h1r0ku.dto.response;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class ProductResponse {
     private Long id;
-    private Double price;
+    private String productName;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
     private CategoryResponse category;
     private List<ProductImageResponse> images;
     private LocalDateTime createdAt;
