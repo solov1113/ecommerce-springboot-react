d6fd1059efdacde6577a4dcfa366b686506f11e7
ericdev0212 <eric.devos0212@gmail.com>
2023-02-26 19:41:34 +0100

    Fix path to customer API

--- order-service/src/main/java/h1r0ku/feign/CustomerClient.java
+++ order-service/src/main/java/h1r0ku/feign/CustomerClient.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import static h1r0ku.constants.FeignConstants.CUSTOMER_SERVICE;
+import static h1r0ku.constants.PathConstants.API_V1_CUSTOMER;
 
 @FeignClient(value = CUSTOMER_SERVICE, configuration = FeignClientConfiguration.class)
 public interface CustomerClient {
-    @GetMapping("/{customerId}")
+    @GetMapping(API_V1_CUSTOMER + "/{customerId}")
     ResponseEntity<CustomerResponse> getById(@PathVariable("customerId") Long id);
 }
