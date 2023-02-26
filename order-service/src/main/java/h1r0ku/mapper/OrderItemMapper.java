bb4ee1c53647d2ffdd042d36f30d2b03f1921091
ericdev0212 <eric.devos0212@gmail.com>
2023-02-26 19:38:04 +0100

    Add order item to order

--- order-service/src/main/java/h1r0ku/mapper/OrderItemMapper.java
+++ order-service/src/main/java/h1r0ku/mapper/OrderItemMapper.java
@@ -1,13 +1,18 @@
 package h1r0ku.mapper;
 
+import h1r0ku.dto.catalog.product.ProductResponse;
 import h1r0ku.dto.request.OrderItemRequest;
 import h1r0ku.dto.response.OrderItemResponse;
+import h1r0ku.dto.response.OrderResponse;
 import h1r0ku.entity.Order;
 import h1r0ku.entity.OrderItem;
+import h1r0ku.feign.ProductClient;
 import h1r0ku.service.OrderItemService;
 import h1r0ku.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 @RequiredArgsConstructor
@@ -16,13 +21,29 @@ public class OrderItemMapper {
     private final BasicMapper basicMapper;
     private final OrderItemService orderItemService;
     private final OrderService orderService;
+    private final ProductClient productClient;
 
-    public OrderItemResponse create(OrderItemRequest orderItemRequest) {
+    public OrderResponse create(Long orderId, OrderItemRequest orderItemRequest) {
+        Long productId = orderItemRequest.getProductId();
+        ProductResponse product = productClient.getProductById(productId).getBody();
+
         OrderItem orderItem = basicMapper.convertTo(orderItemRequest, OrderItem.class);
-        Order order = orderService.getOrderById(orderItemRequest.getOrderId());
+        Order order = orderService.getOrderById(orderId);
         orderItem.setOrder(order);
-        return basicMapper.convertTo(orderItemService.create(orderItem), OrderItemResponse.class);
+        order.increaseOrderFee(product.getPrice().multiply(BigDecimal.valueOf(orderItemRequest.getQuantity())));
+
+        for(OrderItem item : order.getOrderItems()) {
+            if (item.getProductId().equals(productId)) {
+                item.setQuantity(item.getQuantity() + orderItem.getQuantity());
+                orderItemService.updateOrderItem(item.getId(), item);
+            }
+        }
+
+        orderItemService.create(orderItem);
+        Order updatedOrder = orderService.updateOrder(orderId, order);
+        return basicMapper.convertTo(updatedOrder, OrderResponse.class);
     }
+
     public OrderItemResponse updateOrderItem(Long id, OrderItemRequest updatedOrderItem) {
         OrderItem orderItem = basicMapper.convertTo(updatedOrderItem, OrderItem.class);
         return basicMapper.convertTo(orderItemService.updateOrderItem(id, orderItem), OrderItemResponse.class);
