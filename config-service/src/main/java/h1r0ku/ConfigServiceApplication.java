c0292f92fd3f6130bd5ecbb4209c14519dec3750
ericdev0212 <eric.devos0212@gmail.com>
2023-02-26 14:26:36 +0100

    Config service init

---
+++ config-service/src/main/java/h1r0ku/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package h1r0ku;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
