52da63e7bafbdd3d15dcc2c8419844ea22c38eb4
ericdev0212 <eric.devos0212@gmail.com>
2023-02-27 19:09:16 +0100

    Update authentication

---
+++ auth-service/src/main/java/h1r0ku/config/ApplicationConfig.java
@@ -0,0 +1,43 @@
+package h1r0ku.config;
+
+import h1r0ku.feign.CustomerClient;
+import h1r0ku.model.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final CustomerClient customerClient;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> new UserDetailsImpl(customerClient.getByUsername(username).getBody());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- auth-service/src/main/java/h1r0ku/security/SecurityConfig.java
+++ auth-service/src/main/java/h1r0ku/security/SecurityConfig.java
@@ -5,8 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationProvider authenticationProvider;
     private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
@@ -38,12 +38,8 @@ public class SecurityConfig {
                         .sessionManagement()
                             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and()
+                        .authenticationProvider(authenticationProvider)
                         .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                     .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
     }
 }
