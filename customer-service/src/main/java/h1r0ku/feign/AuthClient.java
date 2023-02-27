21ed9bbbf2f6bd654abd3586de80ccf82b12500b
ericdev0212 <eric.devos0212@gmail.com>
2023-02-27 19:22:03 +0100

    Change return type

--- customer-service/src/main/java/h1r0ku/feign/AuthClient.java
+++ customer-service/src/main/java/h1r0ku/feign/AuthClient.java
@@ -2,9 +2,7 @@ package h1r0ku.feign;
 
 import h1r0ku.configuration.FeignClientConfiguration;
 import h1r0ku.dto.authentication.AuthenticationRequest;
-import h1r0ku.dto.authentication.AuthenticationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,5 +12,5 @@ import static h1r0ku.constants.PathConstants.API_V1_AUTH;
 @FeignClient(value = AUTH_SERVICE, configuration = FeignClientConfiguration.class)
 public interface AuthClient {
     @PostMapping(API_V1_AUTH)
-    ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest);
+    void authenticate(@RequestBody AuthenticationRequest authenticationRequest);
 }
