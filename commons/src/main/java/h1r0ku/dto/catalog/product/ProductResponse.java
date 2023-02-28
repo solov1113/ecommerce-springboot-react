33c27ff0b2c4eb5079f4ad4034dea519f583425d
ericdev0212 <eric.devos0212@gmail.com>
2023-02-28 18:57:21 +0100

    Ignore category products

--- commons/src/main/java/h1r0ku/dto/catalog/product/ProductResponse.java
+++ commons/src/main/java/h1r0ku/dto/catalog/product/ProductResponse.java
@@ -1,5 +1,6 @@
 package h1r0ku.dto.catalog.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import h1r0ku.dto.catalog.category.CategoryResponse;
 import lombok.Data;
 
@@ -14,6 +15,7 @@ public class ProductResponse {
     private String description;
     private BigDecimal price;
     private Integer quantity;
+    @JsonIgnoreProperties("products")
     private CategoryResponse category;
     private List<ProductImageResponse> images;
     private LocalDateTime createdAt;
