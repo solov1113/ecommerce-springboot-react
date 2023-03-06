9480bd07e58cc04cbe9f60155e08825af75a3266
ericdev0212 <eric.devos0212@gmail.com>
2023-03-06 17:35:54 +0100

    Frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home";
+import ProductListPage from "./pages/Product/ProductListPage";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductListPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
