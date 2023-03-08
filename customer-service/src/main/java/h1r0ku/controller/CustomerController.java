c69dc96b6bb21a3abc9641f5217099665df73dfa
ericdev0212 <eric.devos0212@gmail.com>
2023-03-08 18:35:23 +0100

    Shipping address routes

--- customer-service/src/main/java/h1r0ku/controller/CustomerController.java
+++ customer-service/src/main/java/h1r0ku/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package h1r0ku.controller;
 
 import h1r0ku.dto.request.CustomerRequest;
+import h1r0ku.dto.request.ShippingAddressRequest;
 import h1r0ku.dto.response.CustomerResponse;
+import h1r0ku.dto.response.ShippingAddressResponse;
 import h1r0ku.mapper.CustomerMapper;
+import h1r0ku.mapper.ShippingAddressMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -17,6 +20,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/customers")
@@ -24,6 +29,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class CustomerController {
 
     private final CustomerMapper customerMapper;
+    private final ShippingAddressMapper shippingAddressMapper;
 
     @PostMapping
     @Operation(summary = "Create a new customer", description = "Create a new customer")
@@ -88,6 +94,64 @@ public class CustomerController {
     })
     public ResponseEntity<Void> deleteById(@PathVariable("customerId") Long id) {
         customerMapper.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+//    Shipping Address
+    @PostMapping("/shipping-address")
+    @Operation(summary = "Create a new shipping address", description = "Create a new shipping address")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipping address created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<ShippingAddressResponse> createShippingAddress(@Valid @RequestBody ShippingAddressRequest address) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(shippingAddressMapper.createShippingAddress(address));
+    }
+
+    @GetMapping("/shipping-address/{id}")
+    @Operation(summary = "Get a shipping address by ID", description = "Get a shipping address by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipping address retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Shipping address not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<ShippingAddressResponse> getShippingAddressById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(shippingAddressMapper.getShippingAddressById(id));
+    }
+
+    @GetMapping("/{id}/shipping-address/")
+    @Operation(summary = "Get all shipping addresses by customer ID", description = "Get all shipping addresses by customer ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipping addresses retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<List<ShippingAddressResponse>> getAllShippingAddressesByCustomerId(@PathVariable("id") Long customerId) {
+        return ResponseEntity.ok(shippingAddressMapper.getAllShippingAddressesByCustomerId(customerId));
+    }
+
+    @PutMapping("/shipping-address/{id}")
+    @Operation(summary = "Update a shipping address by ID", description = "Update a shipping address by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipping address updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body"),
+            @ApiResponse(responseCode = "404", description = "Shipping address not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<ShippingAddressResponse> updateShippingAddress(@PathVariable("id") Long id,
+                                                                 @Valid @RequestBody ShippingAddressRequest address) {
+        return ResponseEntity.ok(shippingAddressMapper.updateShippingAddress(id, address));
+    }
+
+    @DeleteMapping("/shipping-address/{id}")
+    @Operation(summary = "Delete a shipping address by ID", description = "Delete a shipping address by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Shipping address deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Shipping address not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Void> deleteShippingAddress(@PathVariable("id") Long id) {
+        shippingAddressMapper.deleteShippingAddress(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
