e8cd19aae1a6b9a820ea0b002a7f38ba6a3a63de
ericdev0212 <eric.devos0212@gmail.com>
2023-03-09 19:09:02 +0100

    Add tests

---
+++ catalog-service/src/test/java/h1r0ku/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,258 @@
+package h1r0ku.service.impl;
+
+import h1r0ku.entity.Category;
+import h1r0ku.entity.Product;
+import h1r0ku.entity.ProductImage;
+import h1r0ku.feign.ImageClient;
+import h1r0ku.repository.ProductImageRepository;
+import h1r0ku.repository.ProductRepository;
+import h1r0ku.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private ProductImageRepository productImageRepository;
+    @Mock
+    private CategoryService categoryService;
+    @Mock
+    private ImageClient imageClient;
+
+    private ProductServiceImpl serviceTest;
+
+    private static final int MIN_REVIEWS = 10;
+    private static final int NORMALIZATION_FACTOR = 100;
+
+    @BeforeEach
+    public void setup() {
+        serviceTest = new ProductServiceImpl(
+                productRepository,
+                productImageRepository,
+                categoryService,
+                imageClient
+        );
+    }
+
+    @Test
+    public void create_shouldCreateProductWithGivenCategoryId() {
+        // Given
+        Product product = new Product();
+        product.setProductName("Product");
+        product.setDescription("Product description");
+        Long categoryId = 1L;
+
+        Category category = new Category();
+        category.setId(categoryId);
+
+        when(categoryService.getCategoryById(categoryId)).thenReturn(category);
+        when(productRepository.save(any(Product.class))).thenReturn(product);
+
+        // When
+        Product createdProduct = serviceTest.create(product, categoryId);
+
+        // Then
+        assertNotNull(createdProduct);
+        assertEquals(category, createdProduct.getCategory());
+    }
+
+    @Test
+    public void update_shouldUpdateProductWithGivenIdAndCategoryId() {
+        // Given
+
+
+        Long categoryId = 2L;
+
+        Category oldCategory = new Category();
+        Set<Product> products = oldCategory.getProducts();
+        oldCategory.setId(1L);
+        oldCategory.setProducts(products);
+
+        Long productId = 1L;
+        Product existingProduct = new Product();
+        existingProduct.setId(productId);
+        existingProduct.setProductName("Product");
+        existingProduct.setDescription("Product description");
+        existingProduct.setCategory(oldCategory);
+        products.add(existingProduct);
+
+
+        Product updatedProduct = new Product();
+        updatedProduct.setProductName("Updated product");
+        updatedProduct.setDescription("Updated product description");
+
+        Category category = new Category();
+        category.setId(categoryId);
+
+        when(productRepository.findById(productId)).thenReturn(java.util.Optional.of(existingProduct));
+        when(categoryService.getCategoryById(categoryId)).thenReturn(category);
+        when(categoryService.getCategoryById(oldCategory.getId())).thenReturn(oldCategory);
+        when(productRepository.save(any(Product.class))).thenReturn(updatedProduct);
+
+        // When
+        Product result = serviceTest.update(productId, updatedProduct, categoryId);
+
+        // Then
+        assertEquals(updatedProduct.getProductName(), result.getDescription());
+        assertEquals(updatedProduct.getDescription(), result.getDescription());
+        assertEquals(category, result.getCategory());
+    }
+
+    @Test
+    void update_shouldUpdateGivenProduct() {
+        // Given
+        Long productId = 1L;
+        Long categoryId = 2L;
+        Product product = new Product();
+        product.setProductName("Product");
+        product.setId(productId);
+        Category category = new Category();
+        category.setId(categoryId);
+        category.setName("Category");
+        product.setCategory(category);
+
+        Product existingProduct = new Product();
+        existingProduct.setId(productId);
+        existingProduct.setProductName("Existing Product");
+        existingProduct.setCategory(category);
+
+        when(productRepository.findById(productId)).thenReturn(Optional.of(existingProduct));
+        when(categoryService.getCategoryById(categoryId)).thenReturn(category);
+        when(productRepository.save(any(Product.class))).thenReturn(product);
+
+        // When
+        Product updatedProduct = serviceTest.update(productId, product, categoryId);
+
+        // Then
+        verify(productRepository, times(1)).findById(productId);
+        verify(categoryService, times(1)).getCategoryById(categoryId);
+        verify(productRepository, times(1)).save(any(Product.class));
+
+        assertNotNull(updatedProduct);
+        assertEquals(updatedProduct.getId(), productId);
+        assertEquals(updatedProduct.getProductName(), product.getProductName());
+        assertEquals(updatedProduct.getCategory(), product.getCategory());
+        assertEquals(updatedProduct.getCategory().getId(), product.getCategory().getId());
+    }
+
+    @Test
+    public void uploadImages_shouldUploadProductImages() {
+        // Given
+        Long productId = 1L;
+        MultipartFile[] images = new MultipartFile[] {
+                new MockMultipartFile("image", "test.jpg", "image/jpeg", "test image".getBytes()),
+                new MockMultipartFile("image", "test2.jpg", "image/jpeg", "test2 image".getBytes())
+        };
+        Product product = new Product();
+        product.setId(productId);
+        when(productRepository.findById(productId)).thenReturn(java.util.Optional.of(product));
+        when(productImageRepository.save(any(ProductImage.class))).thenReturn(new ProductImage());
+
+        // When
+        Product result = serviceTest.uploadImages(images, productId);
+
+        // Then
+        assertEquals(result.getImages().size(), images.length);
+    }
+
+    @Test
+    public void updateOrderCount_shouldUpdateOrderCount() {
+        // Given
+        Long productId = 1L;
+        when(productRepository.findById(productId)).thenReturn(java.util.Optional.of(new Product()));
+
+        // When
+        serviceTest.updateOrderCount(productId, true);
+
+        // Then
+        // verify that productRepository.updateOrderCount was called with productId and true
+    }
+
+    private static Float calculatePopularityScore(Product product) {
+        Float avgRating = product.getAverageStar();
+        int numReviews = product.getReviews().size();
+
+        return (MIN_REVIEWS / (MIN_REVIEWS + (float)numReviews)) * avgRating
+                + (numReviews / (MIN_REVIEWS + (float)numReviews)) * NORMALIZATION_FACTOR;
+    }
+
+    @Test
+    public void getAll_shouldReturnAllProductsSortedByPopularity() {
+        // Given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+
+        Comparator<Product> comparator = (p1, p2) -> {
+            Float p1Score = calculatePopularityScore(p1);
+            Float p2Score = calculatePopularityScore(p2);
+            return p2Score.compareTo(p1Score);
+        };
+
+        when(productRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(products));
+        List<Product> sortedProducts = products.stream().sorted(comparator).toList();
+        when(productRepository.findAll(any(Pageable.class)).stream().sorted(comparator)).thenReturn(sortedProducts.stream());
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Order.desc("popularity")));
+
+        // When
+        Page<Product> result = serviceTest.getAll(pageable);
+
+        // Then
+        assertEquals(result.getContent(), products);
+    }
+
+    @Test
+    public void getAll_shouldReturnAllProducts() {
+        // Given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+        when(productRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(products));
+
+        // When
+        Page<Product> result = serviceTest.getAll(Pageable.unpaged());
+
+        // Then
+        assertEquals(result.getContent(), products);
+    }
+
+    @Test
+    public void getById_shouldReturnProductById() {
+        // Given
+        Long productId = 1L;
+        Product product = new Product();
+        product.setId(productId);
+        when(productRepository.findById(productId)).thenReturn(java.util.Optional.of(product));
+
+        // When
+        Product result = serviceTest.getById(productId);
+
+        // Then
+        assertEquals(result, product);
+    }
+
+    @Test
+    public void deleteProduct_shouldDeleteProductById() {
+        // Given
+        Long productId = 1L;
+
+        // When
+        serviceTest.deleteProduct(productId);
+
+        // Then
+        verify(productRepository).deleteById(productId);
+    }
+}
---
+++ catalog-service/src/test/java/h1r0ku/service/impl/ReviewServiceImplTest.java
@@ -0,0 +1,186 @@
+package h1r0ku.service.impl;
+
+import h1r0ku.entity.Product;
+import h1r0ku.entity.Review;
+import h1r0ku.feign.CustomerClient;
+import h1r0ku.repository.ReviewRepository;
+import h1r0ku.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewServiceImplTest {
+
+    @Mock
+    private ReviewRepository reviewRepository;
+    @Mock
+    private CustomerClient customerClient;
+    @Mock
+    private ProductService productService;
+
+    private ReviewServiceImpl serviceTest;
+
+    @BeforeEach
+    public void setup() {
+        serviceTest = new ReviewServiceImpl(
+                reviewRepository,
+                customerClient,
+                productService
+        );
+    }
+
+    @Test
+    void create_givenValidProductAndReview_shouldCreateReview() {
+        // Given
+        Long productId = 1L;
+        Long customerId = 2L;
+        Product product = new Product();
+        product.setId(productId);
+        List<Review> reviews = new ArrayList<>();
+        Review existingReview = new Review();
+        existingReview.setRating((short) 4);
+        existingReview.setProduct(product);
+        reviews.add(existingReview);
+        product.setReviews(reviews);
+        Review review = new Review();
+        review.setCustomerId(customerId);
+        review.setRating((short) 5);
+        when(productService.getById(productId)).thenReturn(product);
+        when(customerClient.getById(customerId)).thenReturn(ResponseEntity.ok(new Object()));
+        when(reviewRepository.save(any(Review.class))).thenReturn(review);
+
+        // When
+        Review result = serviceTest.create(productId, review);
+
+        // Then
+        verify(customerClient, times(1)).getById(customerId);
+        verify(productService, times(1)).getById(productId);
+        verify(reviewRepository, times(1)).save(any(Review.class));
+        assertEquals(result, review);
+        assertEquals(product.getAverageStar(), 4.5f);
+    }
+
+    @Test
+    void update_givenValidReviewIdAndProduct_shouldUpdateReview() {
+        // Given
+        Long productId = 1L;
+        Long reviewId = 2L;
+        Review existingReview = new Review();
+        existingReview.setRating((short) 4);
+        existingReview.setProduct(new Product());
+        existingReview.setId(reviewId);
+        Review newReview = new Review();
+        newReview.setRating((short) 3);
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(existingReview));
+        when(productService.getById(productId)).thenReturn(existingReview.getProduct());
+        when(reviewRepository.save(any(Review.class))).thenReturn(newReview);
+
+        // When
+        Review result = serviceTest.update(reviewId, productId, newReview);
+
+        // Then
+        verify(productService, times(1)).getById(productId);
+        verify(reviewRepository, times(1)).findById(reviewId);
+        verify(reviewRepository, times(1)).save(any(Review.class));
+        assertEquals(result.getRating(), newReview.getRating());
+    }
+
+    @Test
+    void getById_givenValidReviewId_shouldReturnReview() {
+        // Given
+        Long reviewId = 1L;
+        Review review = new Review();
+        review.setId(reviewId);
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+        // When
+        Review result = serviceTest.getById(reviewId);
+
+        // Then
+        verify(reviewRepository, times(1)).findById(reviewId);
+        assertEquals(result, review);
+    }
+
+    @Test
+    public void getByProductId_shouldReturnPageOfReviews() {
+        // Given
+        Long productId = 1L;
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<Review> reviews = new ArrayList<>();
+        Product product = new Product();
+        product.setId(productId);
+        for (int i = 1; i <= 10; i++) {
+            Review review = new Review();
+            review.setId((long) i);
+            review.setProduct(product);
+            reviews.add(review);
+        }
+
+        Page<Review> expectedPage = new PageImpl<>(reviews, pageable, reviews.size());
+
+        when(reviewRepository.findByProduct_Id(productId, pageable)).thenReturn(expectedPage);
+
+        // When
+        Page<Review> result = serviceTest.getByProductId(productId, pageable);
+
+        // Then
+        assertEquals(result, expectedPage);
+        verify(reviewRepository).findByProduct_Id(productId, pageable);
+    }
+
+    @Test
+    public void getByCustomerId_shouldReturnPageOfReviews() {
+        // Given
+        Long customerId = 1L;
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<Review> reviews = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            Review review = new Review();
+            review.setId((long) i);
+            review.setCustomerId(customerId);
+            reviews.add(review);
+        }
+
+        Page<Review> expectedPage = new PageImpl<>(reviews, pageable, reviews.size());
+
+        when(reviewRepository.findByCustomerId(customerId, pageable)).thenReturn(expectedPage);
+
+        // When
+        Page<Review> result = serviceTest.getByCustomerId(customerId, pageable);
+
+        // Then
+        assertEquals(result, expectedPage);
+        verify(reviewRepository).findByCustomerId(customerId, pageable);
+    }
+
+    @Test
+    public void deleteById_shouldDeleteReviewById() {
+        // Given
+        Long reviewId = 1L;
+
+        // When
+        serviceTest.deleteById(reviewId);
+
+        // Then
+        verify(reviewRepository).deleteById(reviewId);
+    }
+}
